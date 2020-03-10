793e25e1e1663411539a6804c46f6d43f280175a
Alexandre Toshiro <atoshiro@indracompany.com>
2020-03-10 09:33:09 -0300

    Properties do banco / Notações JPA nos models / Banco h2

--- src/main/java/br/com/alura/forum/model/Resposta.java
+++ src/main/java/br/com/alura/forum/model/Resposta.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Resposta {
@@ -13,8 +14,12 @@ public class Resposta {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
 	private String mensagem;
+	
+	@ManyToOne
 	private Topico topico;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+	
+	@ManyToOne
 	private Usuario autor;
 	private Boolean solucao = false;
 
